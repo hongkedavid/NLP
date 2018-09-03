0bca76ea1a42d6e3b5d61802145e1ca0ff9c7cc4
David Ke Hong <hongkedavid@gmail.com>
2018-09-02 20:59:06 -0400

    Update crawl_log.sh

--- commit_crawl/crawl_log.sh
+++ commit_crawl/crawl_log.sh
@@ -1,2 +1,13 @@
 # Ref: https://git-scm.com/book/en/v2/Git-Basics-Viewing-the-Commit-History
+
+# Last 10 commits
+git log -10
+
+# Get details of certain commit
+git log -p $commit_id
+
+# Ref: https://git-scm.com/docs/pretty-formats
 git log --pretty=oneline
+git log --pretty=format:"%H - %s - %b"
+
+git log --pretty="%h - %s" --author='$author' --since="2008-10-01" --before="2008-11-01" --no-merges -- t/
