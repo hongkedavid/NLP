346eebb87e7502e4fa7a4fda1952ebe6b938fe33
David Ke Hong <hongkedavid@gmail.com>
2018-10-17 18:39:10 -0400

    Create run_nlp_analyzer.sh

---
+++ run_nlp_analyzer.sh
@@ -0,0 +1,11 @@
+# Parse commit logs, manpage or specification
+cat commit.log.all | grep -v "Author: \|Date: \|commit " | python parse_tree_commit.py > commit.full
+cat manpage | python parse_tree.py > manpage.filter
+cat tcpros.doc | python parse_tree.py > tcpros.doc.out
+cat tcpros.manpage | python parse_tree.py > tcpros.manpage.out
+cat rfc1035.txt | head -n2736 | tail -n2608 | python parse_tree_rfc.py > rfc1035.filter
+
+# Topic modeling
+cat commit.full | python topic_model.py
+cat manpage.filter | python topic_model.py
+cat tcpros.manpage.out tcpros.doc.out | python topic_model.py
